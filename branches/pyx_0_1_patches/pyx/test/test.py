a73ad2e89e2fa34a488283b213ddc0c732a73aa5
nobody <nobody@a4f5e268-e194-4f32-bce1-d30804cbbcc5>
2002-10-16 14:48:09 +0000

    This commit was manufactured by cvs2svn to create branch 'pyx_0_1_patches'.

---
+++ branches/pyx_0_1_patches/pyx/test/test.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+#
+#
+# Copyright (C) 2002 Jörg Lehmann <joergl@users.sourceforge.net>
+# Copyright (C) 2002 André Wobst <wobsta@users.sourceforge.net>
+#
+# This file is part of PyX (http://pyx.sourceforge.net/).
+#
+# PyX is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# PyX is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyX; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import sys
+sys.path.append("..")
+
+import unittest
+from pyx import *
+
+def isEqual(trafo1, trafo2):
+    return max(map(abs,[trafo1.matrix[0][0]-trafo2.matrix[0][0],
+                        trafo1.matrix[0][1]-trafo2.matrix[0][1],
+                        trafo1.matrix[1][0]-trafo2.matrix[1][0],
+                        trafo1.matrix[1][1]-trafo2.matrix[1][1],
+                        trafo1.vector[0]-trafo2.vector[0],
+                        trafo1.vector[1]-trafo2.vector[1]]))<1e-7
+
+def correctOnBasis(t, tesx, tesy):
+    esx=unit.convert_to(t.apply(1,0), "cm")
+#    esx=esx[0].convert_to("cm"), esx[1].convert_to("cm")
+    
+    esy=unit.convert_to(t.apply(0,1), "cm")
+#    esy=esy[0].convert_to("cm"), esy[1].convert_to("cm")
+    
+#    print "  (1,0) => (%f, %f)" % (unit.tom(esx[0])*100, unit.tom(esx[1])*100)
+#    print "  (0,1) => (%f, %f)" % (unit.tom(esy[0])*100, unit.tom(esy[1])*100)
+    
+    return max(map(abs,[esx[0]-tesx[0], esx[1]-tesx[1],
+                        esy[0]-tesy[0], esy[1]-tesy[1]]))<1e-7
+
+
+class TrafoTestCase(unittest.TestCase):
+
+    def testInverse(self):
+        "t*t.inverse()=1"
+        t = trafo.translation(-1,-1)*trafo.rotation(72)*trafo.translation(1,1)
+        assert isEqual(t*t.inverse(), trafo.trafo()), \
+                "wrong inverse definition"
+
+    def testTranslate(self):
+        assert correctOnBasis(trafo.translation(1,0),
+                              (2,0), (1,1)), \
+               "wrong definition of trafo.translation"
+        assert correctOnBasis(trafo.translation(0,1),
+                              (1,1), (0,2)), \
+               "wrong definition of trafo.translation"
+
+    def testRotate(self):
+        assert correctOnBasis(trafo.rotation(90),
+                              (0,1), (-1,0)), \
+               "wrong definition of trafo.rotation"
+        assert isEqual(trafo.rotation(360), trafo.trafo()), \
+                "trafo.rotation by 360 deg is not 1"
+        assert isEqual(trafo.rotation(40)*trafo.rotation(120)*
+                       trafo.rotation(90)*trafo.rotation(110),
+                        trafo.trafo()), \
+                "successive multiplication by 360 degrees does not yield 1"
+
+    def testMirror(self):
+        "trafo.mirroring two times must yield 1 and -mirror(phi)=mirror(phi+180)"
+        assert isEqual(trafo.mirroring(20)*trafo.mirroring(20),
+                       trafo.trafo()), \
+                "trafo.mirroring not idempotent"
+        assert isEqual(trafo.mirroring(20),
+                       trafo.mirroring(180+20)), \
+                "trafo.mirroring by 20 degrees unequal to trafo.mirroring by 180+20 degrees"
+
+    def testScale(self):
+        assert correctOnBasis(trafo.scaling(0.5),
+                              (0.5,0), (0, 0.5)), \
+               "wrong definition of trafo.scaling"
+        assert correctOnBasis(trafo.scaling(0.5, 0.2),
+                              (0.5,0), (0, 0.2)), \
+               "wrong definition of trafo.scaling"
+        assert isEqual(trafo.scaling(2,3)*trafo.scaling(1/2.0, 1/3.0),
+                       trafo.trafo()), \
+                "trafo.scaling definition wrong"
+
+   
+    def testMultVsMethods(self):
+        "test multiplication vs trafo methods"
+        assert isEqual(trafo.rotation(72).translate(1,2),
+                       trafo.translation(1,2)*trafo.rotation(72)), \
+               "trafo.translate not consistent with multiplication result"
+        assert isEqual(trafo.mirroring(20)*trafo.mirroring(20),
+                       trafo.mirroring(20).mirror(20)), \
+               "trafo.mirror not consistent with multiplication result"
+        assert isEqual(trafo.translation(1,2).rotate(72).translate(-3,-1),
+                       trafo.translation(-3,-1)*
+                       trafo.rotation(72)*
+                       trafo.translation(1,2)), \
+                "trafo.translate/rotate not consistent with multiplication result"
+   
+    def testTranslateCombined(self):
+        assert correctOnBasis(trafo.translation(1,0)*trafo.rotation(90),
+                              (1,1), (0,0)), \
+               "wrong trafo.translation/trafo.rotation definition"
+        assert correctOnBasis(trafo.rotation(90)*trafo.translation(1,0),
+                              (0,2), (-1,1)), \
+               "wrong trafo.translation/trafo.rotation definition"
+        assert isEqual(trafo.rotation(72,1,2),
+                       trafo.translation(-1,-2).rotate(72).translate(1,2)), \
+               "wrong translate/rotate definition"
+        
+        assert correctOnBasis(trafo.translation(1,0)*
+                              trafo.scaling(0.5),
+                              (1.5, 0), (1.0,0.5)), \
+               "wrong trafo.translation/trafo.scaling definition"
+        assert correctOnBasis(trafo.scaling(0.5)*
+                              trafo.translation(1,0),
+                              (1, 0), (0.5,0.5)), \
+               "wrong trafo.translation/trafo.scaling definition"
+                
+        assert correctOnBasis(trafo.translation(1,0)*
+                              trafo.rotation(90)*
+                              trafo.scaling(0.5),
+                              (1,0.5), (0.5,0)), \
+               "wrong trafo.translation/trafo.rotation/trafo.scaling definition"
+        assert correctOnBasis(trafo.translation(1,0)*
+                              trafo.scaling(0.5)*
+                              trafo.rotation(90),
+                              (1,0.5), (0.5,0)), \
+               "wrong trafo.translation/trafo.rotation/trafo.scaling definition"
+        assert correctOnBasis(trafo.rotation(90)*trafo.scaling(0.5)*trafo.translation(1,0),
+                              (0,1), (-0.5,0.5)), \
+               "wrong trafo.translation/trafo.rotation/trafo.scaling definition"
+
+class UnitTestCase(unittest.TestCase):
+    def testTrueUnits(self):
+        assert unit.topt(unit.t_pt(42)) == 42, "wrong unit definition"
+        assert unit.tom(unit.t_m(42)) == 42, "wrong unit definition"
+
+    def testUserUnits(self):
+        unit.set(uscale=2)
+        assert unit.topt(unit.pt(42)) == 84, "wrong unit definition"
+        assert unit.tom(unit.m(42)) == 84, "wrong unit definition"
+      
+
+# construct the test suite automagically
+
+suite = unittest.TestSuite((unittest.makeSuite(TrafoTestCase, 'test'),
+                            unittest.makeSuite(UnitTestCase, 'test')))
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
+    
+
+
+
---
+++ branches/pyx_0_1_patches/pyx/test/test_graph.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+import sys, math
+sys.path.append("..")
+from pyx import *
+from pyx import mathtree
+
+
+def test_multiaxes_data(c, t, x, y):
+    g = c.insert(graph.graphxy(t, x, y, height=5,
+                               x=graph.logaxis(title="$W$", part=graph.autologpart(mix=graph.manualpart(ticks="2.2360679775", texts="$\sqrt{5}$").part())),
+                               y=graph.logaxis(title=r"$PPP_1$",
+                                               painter=graph.axispainter(titleattrs=tex.direction.horizontal)),
+                               y2=graph.logaxis(title="$P_2$"),
+                               y3=graph.logaxis(title="$PPP_3$",
+                                                painter=graph.axispainter(titleattrs=tex.direction(45))),
+                               y5=graph.logaxis(title="$P_5$")))
+    df = datafile.datafile("testdata")
+    g.plot((graph.data(df, x=1, y="sqrt(sqrt($3))"),
+            graph.data(df, x=1, y2=4),
+            graph.data(df, x=1, y3=5),
+            graph.data(df, x=1, y5=6)),
+           style=graph.mark(markattrs=(graph.changecolor.RedGreen(), graph.changestrokedfilled()), mark=graph.changemark.squaretwice()))
+    g.finish()
+
+def test_piaxis_function(c, t, x, y):
+    g = c.insert(graph.graphxy(t, x, y, height=5,
+                               x=graph.linaxis(min=0, max=2*math.pi, divisor=math.pi, suffix=r"\pi")))
+    g.plot([graph.function("y=sin(x-%i*pi/10)" % i, points=100) for i in range(20)],
+           style=graph.line(lineattrs=(graph.changecolor.Hue(), graph.changelinestyle())))
+    g.finish()
+
+def test_textaxis_errorbars(c, t, x, y):
+    df = datafile.datafile("testdata2")
+    g = c.insert(graph.graphxy(t, x, y, height=5,
+                               x=graph.linaxis(min=0.5, max=12.5, title="Month",
+                                               part=graph.linpart("1", texts=df.getcolumn("month"), extendtick=None),
+                                               painter=graph.axispainter(labeldist=0.1, titledist=0, labelattrs=(tex.direction(45),tex.halign.right, tex.fontsize.scriptsize))),
+                               y=graph.linaxis(min=-10, max=30, title="Temperature [$^\circ$C]"),
+                               x2=graph.linaxis(), y2=graph.linaxis()))
+    g.plot(graph.data(df, x=0, ymin="min", ymax="max"))
+    g.plot(graph.paramfunction("k", 0, 2*math.pi, "x2, y2, dx2, dy2 = 0.8*sin(k), 0.8*cos(3*k), 0.05, 0.05"), style = graph.mark(mark=graph.mark.triangle))
+    g.finish()
+
+def test_ownmark(c, t, x, y):
+
+    class Div(mathtree.MathTreeFunc2):
+        def __init__(self, *args):
+            mathtree.MathTreeFunc2.__init__(self, "div", *args)
+        def Calc(self, VarDict):
+            return divmod(self.ArgV[0].Calc(VarDict), self.ArgV[1].Calc(VarDict))[0]
+
+    class Mod(mathtree.MathTreeFunc2):
+        def __init__(self, *args):
+            mathtree.MathTreeFunc2.__init__(self, "mod", *args)
+        def Calc(self, VarDict):
+            return divmod(self.ArgV[0].Calc(VarDict), self.ArgV[1].Calc(VarDict))[1]
+
+    MyFuncs = mathtree.DefaultMathTreeFuncs + (Div, Mod)
+
+    g = c.insert(graph.graphxy(t, x, y, height=5, x=graph.linaxis(min=0, max=10), y=graph.linaxis(min=0, max=10)))
+    g.plot(graph.paramfunction("k", 0, 120, "x, y, size, angle = mod(k, 11), div(k, 11), (1+sin(k*pi/120))/2, 3*k", points=121, parser=mathtree.parser(MathTreeFuncs=MyFuncs)), style = graph.arrow())
+    line1 = g.plot(graph.function("y=10/x"))
+    line2 = g.plot(graph.function("y=12*x^-1.6"))
+    line3 = g.plot(graph.function("y=7/x"))
+    line4 = g.plot(graph.function("y=25*x^-1.6"))
+    g.finish()
+
+    p1=line1.path
+    p2=line2.path.reversed()
+    p3=line3.path.reversed()
+    p4=line4.path
+    (seg1a,), (seg2a,) = p1.intersect(p2)
+    (seg2b,), (seg3b,) = p2.intersect(p3)
+    (seg3c,), (seg4c,) = p3.intersect(p4)
+    (seg4d,), (seg1d,) = p4.intersect(p1)
+    area = p1.split(seg1a, seg1d)[1] << p4.split(seg4d, seg4c)[1] << p3.split(seg3c, seg3b)[1] << p2.split(seg2b, seg2a)[1]
+    area.append(path.closepath())
+    g.stroke(area, canvas.linewidth.THick, canvas.filled(color.gray(0.5)))
+
+def test_allerrorbars(c, t, x, y):
+    df = datafile.datafile("testdata3")
+    g = c.insert(graph.graphxy(t, x, y, height=5, width=4))
+    g.plot(graph.data(df, x="x", y="y", xmin="xmin", xmax="xmax", ymin="ymin", ymax="ymax", text="text"), graph.text())
+    g.finish()
+
+c = canvas.canvas()
+t = c.insert(tex.tex())
+test_multiaxes_data(c, t, 0, 21)
+test_piaxis_function(c, t, 0, 14)
+test_textaxis_errorbars(c, t, 0, 7)
+test_ownmark(c, t, 0, 0)
+test_allerrorbars(c, t, -7, 0)
+
+c.writetofile("test_graph", paperformat="a4")
+
