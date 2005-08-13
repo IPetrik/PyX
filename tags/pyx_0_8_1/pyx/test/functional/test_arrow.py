b03a84224edfa1f19a9c9d631b30880eb86a5dac
nobody <nobody@a4f5e268-e194-4f32-bce1-d30804cbbcc5>
2005-08-13 12:47:05 +0000

    This commit was manufactured by cvs2svn to create tag 'pyx_0_8_1'.

---
+++ tags/pyx_0_8_1/pyx/test/functional/test_arrow.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+import sys; sys.path[:0] = ["../.."]
+
+import pyx
+from pyx import *
+from pyx.path import *
+import math
+
+def testarrow(c):
+    c.stroke(path(moveto(10,20),
+                curveto(12,16,14,15,12,19),
+                rcurveto(-3,2,3,3,-2,4)),
+             [deco.barrow.small, deco.earrow.normal])
+
+    c.stroke(path(arc(8,15,4,10,70)), [deco.barrow.small, deco.earrow.normal])
+    c.stroke(path(arc(8,15,3,10,70)), [deco.barrow.small, deco.earrow.normal])
+    c.stroke(path(arc(8,15,2,10,70)), [deco.barrow.small, deco.earrow.normal])
+    c.stroke(path(arc(8,15,1,10,70)), [deco.barrow.small, deco.earrow.normal])
+    c.stroke(path(arc(8,15,0.5,10,70)), [deco.barrow.small, deco.earrow.normal])
+
+    base = 2
+
+    c.stroke(path(moveto(5,10), rlineto(5,0)),
+           [#deco.barrow(size=base/math.sqrt(8)*unit.t_pt, constriction=1),
+            deco.earrow.SMall])
+    c.stroke(path(moveto(5,10.5), rlineto(5,0)),
+           [deco.barrow(size=base/math.sqrt(4)*unit.t_pt, constriction=1),
+            deco.earrow.Small])
+    c.stroke(path(moveto(5,11), rlineto(5,0)),
+           [deco.barrow(size=base/math.sqrt(2)*unit.t_pt, constriction=1),
+            deco.earrow.small])
+    c.stroke(path(moveto(5,11.5), rlineto(5,0)),
+           [deco.barrow(size=base/math.sqrt(1)*unit.t_pt, constriction=1),
+            deco.earrow.normal])
+    c.stroke(path(moveto(5,12), rlineto(5,0)),
+           [deco.barrow(size=base*math.sqrt(2)*unit.t_pt, constriction=1),
+            deco.earrow.large])
+    c.stroke(path(moveto(5,12.5), rlineto(5,0)),
+           [deco.barrow(size=base*math.sqrt(4)*unit.t_pt, constriction=1),
+            deco.earrow.Large])
+    c.stroke(path(moveto(5,13), rlineto(5,0)),
+           [deco.barrow(size=base*math.sqrt(8)*unit.t_pt, constriction=1),
+            deco.earrow.LArge])
+    c.stroke(path(moveto(5,13.5), rlineto(5,0)),
+           [deco.barrow(size=base*math.sqrt(16)*unit.t_pt, constriction=1),
+            deco.earrow.LARge])
+
+    lt = style.linewidth.THick
+
+    c.stroke(path(moveto(11,10), rlineto(5,0)),
+           [lt,
+            deco.barrow(size=base/math.sqrt(8)*unit.t_pt, constriction=1),
+            deco.earrow.SMall])
+    c.stroke(path(moveto(11,10.5), rlineto(5,0)),
+           [lt,
+            deco.barrow(size=base/math.sqrt(4)*unit.t_pt, constriction=1),
+            deco.earrow.Small])
+    c.stroke(path(moveto(11,11), rlineto(5,0)),
+           [lt,
+            deco.barrow(size=base/math.sqrt(2)*unit.t_pt, constriction=1),
+            deco.earrow.small])
+    c.stroke(path(moveto(11,11.5), rlineto(5,0)),
+           [lt,
+            deco.barrow(size=base/math.sqrt(1)*unit.t_pt, constriction=1),
+            deco.earrow.normal])
+    c.stroke(path(moveto(11,12), rlineto(5,0)),
+           [lt,
+            deco.barrow(size=base*math.sqrt(2)*unit.t_pt, constriction=1),
+            deco.earrow.large])
+    c.stroke(path(moveto(11,12.5), rlineto(5,0)),
+           [lt,
+            deco.barrow(size=base*math.sqrt(4)*unit.t_pt, constriction=1),
+            deco.earrow.Large(attrs=[deco.stroked([style.linestyle.dashed]), deco.filled.clear])])
+    c.stroke(path(moveto(11,13), rlineto(5,0)),
+           [lt,
+            deco.barrow(size=base*math.sqrt(8)*unit.t_pt, constriction=1),
+            deco.earrow.LArge(attrs=[style.linestyle.dashed, color.rgb.green])])
+    c.stroke(path(moveto(11,13.5), rlineto(5,0)),
+           [lt,
+            deco.barrow(size=base*math.sqrt(16)*unit.t_pt, constriction=1),
+            deco.earrow.LARge(attrs=[color.rgb.red,
+                                     deco.stroked([style.linejoin.round]),
+                                     deco.filled([color.rgb.blue])])])
+
+
+
+c=canvas.canvas()
+testarrow(c)
+c.writeEPSfile("test_arrow", paperformat=document.paperformat.A4, rotated=0, fittosize=1)
+c.writePDFfile("test_arrow", paperformat=document.paperformat.A4)
+
---
+++ tags/pyx_0_8_1/pyx/www/pt2html.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+
+import sys, os, os.path, cgi, StringIO, codecs, glob
+import keyword, token, tokenize
+import xml.dom.minidom
+from zope.pagetemplate.pagetemplatefile import PageTemplateFile
+import Image
+
+sys.path[:0]=[".."]
+import pyx
+
+_KEYWORD = token.NT_OFFSET
+
+tokclasses = {token.NUMBER: 'number',
+              token.OP: 'op',
+              token.STRING: 'string',
+              tokenize.COMMENT: 'comment',
+              token.NAME: 'name',
+              _KEYWORD: 'keyword'}
+
+class py2html:
+
+    def __init__(self, input, output):
+        self.output = output
+        self.col = 0
+        self.tokclass = None
+        self.output.write("<pre id=python>")
+        tokenize.tokenize(input.readline, self.tokeneater)
+        if self.tokclass is not None:
+            self.output.write('</span>')
+        self.output.write("</pre>\n")
+
+    def tokeneater(self, toktype, toktext, (srow, scol), (erow, ecol), line):
+        if toktype == token.ERRORTOKEN:
+            raise RuntimeError("ErrorToken occured")
+        if toktype in [token.NEWLINE, tokenize.NL]:
+            self.output.write('\n')
+            self.col = 0
+        else:
+            # map token type to a color group
+            if token.LPAR <= toktype and toktype <= token.OP:
+                toktype = token.OP
+            elif toktype == token.NAME and keyword.iskeyword(toktext):
+                toktype = _KEYWORD
+
+            # restore whitespace
+            assert scol >= self.col
+            self.output.write(" "*(scol-self.col))
+
+            try:
+                tokclass = tokclasses[toktype]
+            except KeyError:
+                tokclass = None
+            if self.tokclass is not None and tokclass != self.tokclass:
+                self.output.write('</span>')
+            if tokclass is not None and tokclass != self.tokclass:
+                self.output.write('<span class="%s">' % tokclass)
+            self.output.write(cgi.escape(toktext))
+            self.tokclass = tokclass
+
+            # calculate new column position
+            self.col = scol + len(toktext)
+            newline = toktext.rfind("\n")
+            if newline != -1:
+                self.col = len(toktext) - newline - 1
+
+
+class example:
+
+    def __init__(self, basename, dir=None):
+        self.title = basename
+        if dir:
+            name = os.path.join(dir, basename)
+        else:
+            name = basename
+        relname = os.path.join("..", "examples", name)
+        htmlbuffer = StringIO.StringIO()
+        py2html(codecs.open("%s.py" % relname, encoding="iso-8859-1"), htmlbuffer)
+        self.code = htmlbuffer.getvalue()
+        self.png = "%s.png" % basename
+        self.width, self.height = Image.open("%s.png" % relname).size
+        self.downloads = []
+        for suffix in ["py", "dat", "jpg", "eps", "pdf"]:
+            try:
+                filesize = "%.1f KB" % (os.path.getsize("%s.%s" % (relname, suffix)) / 1024.0)
+            except OSError:
+                pass
+            else:
+                self.downloads.append({"filename": "%s.%s" % (basename, suffix),
+                                       "suffixname": ".%s" % suffix,
+                                       "filesize": filesize,
+                                       "iconname": "%s.png" % suffix})
+
+
+class MyPageTemplateFile(PageTemplateFile):
+
+    def write(self, text):
+        if isinstance(text, str):
+            text = unicode(text, encoding="iso-8859-1")
+        return PageTemplateFile.write(self, text)
+
+
+def mkrellink(linkname, options):
+    # returns a string containing the relative url for linkname (an absolute url)
+    pagename = options["pagename"]
+    while linkname.find("/") != -1 and pagename.find("/") != -1:
+        linknamefirst, linknameother = linkname.split("/", 1)
+        pagenamefirst, pagenameother = pagename.split("/", 1)
+        if linknamefirst == pagenamefirst:
+            linkname = linknameother
+            pagename = pagenameother
+        else:
+            break
+    for i in pagename.split("/")[:-1]:
+        linkname = "../" + linkname
+    return linkname
+
+maintemplate = MyPageTemplateFile("maintemplate.pt")
+
+latestnews = 2
+newsdom = xml.dom.minidom.parse("news.pt")
+news = "".join(["%s%s" % (dt.toxml(), dd.toxml())
+                for dt, dd in zip(newsdom.getElementsByTagName("dt")[:latestnews],
+                                  newsdom.getElementsByTagName("dd")[:latestnews])])
+
+for ptname in glob.glob("*.pt"):
+    if ptname in ["maintemplate.pt", "examples.pt"]:
+        continue
+    htmlname = "%s.html" % ptname[:-3]
+    template = MyPageTemplateFile(ptname)
+    content = template(pagename=htmlname,
+                       maintemplate=maintemplate,
+                       examplepages=[],
+                       mkrellink=mkrellink,
+                       version=pyx.__version__,
+                       news=news)
+    codecs.open("build/%s" % htmlname, "w", encoding="iso-8859-1").write(content)
+
+examplestemplate = MyPageTemplateFile("examples.pt")
+examplepages = [item[:-2]
+                for item in open("../examples/INDEX").readlines()
+                if item[-2] == "/"]
+
+for dir in [None] + examplepages:
+    srcdir = "../examples"
+    destdir = "examples"
+    if dir:
+        srcdir = os.path.join(srcdir, dir)
+        destdir = os.path.join(destdir, dir)
+    try:
+        abstract = open(os.path.join(srcdir, "README")).read()
+    except IOError:
+        abstract = ""
+    abstract = abstract.replace("__version__", pyx.__version__).replace("\PyX{}", "PyX").replace("\TeX{}", "TeX").replace("\LaTeX{}", "LaTeX").replace("\n\n", "<br><br>")
+    examples = [example(item.strip(), dir)
+                for item in open(os.path.join(srcdir, "INDEX")).readlines()
+                if item[-2] != "/"]
+    htmlname = os.path.join(destdir, "index.html")
+    content = examplestemplate(pagename=htmlname,
+                               maintemplate=maintemplate,
+                               abstract=abstract,
+                               examples=examples,
+                               examplepages=examplepages,
+                               mkrellink=mkrellink)
+    codecs.open("build/%s" % htmlname, "w", encoding="iso-8859-1").write(content)
+
